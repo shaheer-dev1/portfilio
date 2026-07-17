a34e28d7bae0515bad59a05e243f65c49e317336
Shaheer Qureshi <shaheer.qureshi.dev@gmail.com>
2026-07-17 20:49:48 +0500

    Fixed bug

--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:3000',
-        changeOrigin: true,
-      },
-    },
-  },
 })
